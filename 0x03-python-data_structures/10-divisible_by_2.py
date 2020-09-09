08935782ba03d300015c96177164e075c597f870
Lilsherlock <potatogames15@gmail.com>
2020-09-08 23:15:22 -0500

    update

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return my_list
+    div = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            div.append(True)
+        else:
+            div.append(False)
+    return div
