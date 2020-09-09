76c41602afc17af9ecf17ead12576cb77b8844f4
Lilsherlock <potatogames15@gmail.com>
2020-09-08 23:04:48 -0500

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 and idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    for i in range(0, len(my_list)):
-        if idx == i:
-            my_list[i] = element
-            return my_list
+    my_list[idx] = element
+    new_list = my_list[:]
+    return new_list
