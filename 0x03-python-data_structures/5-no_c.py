8c556e9c17445c3b409362018e8aff3a1bc97ebf
Lilsherlock <potatogames15@gmail.com>
2020-09-08 23:09:26 -0500

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
+    if my_string is None:
+        return
     new_string = ""
-    for i in my_string:
-        if i != 'c' and i != 'C':
-            new_string += i
+    for c in range(len(my_string)):
+        if my_string[c] != 'c' and my_string[c] != 'C':
+            new_string = new_string + my_string[c]
     return new_string
