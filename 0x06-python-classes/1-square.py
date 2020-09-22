1b5ebfffe86c2e94fb360fb3309141334b206996
Lilsherlock <potatogames15@gmail.com>
2020-09-22 18:59:03 -0500

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Class square"""
+
+
+class Square:
+    """ init process"""
+
+    def __init__(self, size):
+        """ initialize method
+
+        Args:
+            size(int): Size of square
+        Returns: none
+        """
+        self.__size = size
