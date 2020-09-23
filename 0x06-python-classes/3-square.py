7a0b986f39e87a348b1378bee091486ec49d0fc9
Lilsherlock <potatogames15@gmail.com>
2020-09-22 20:26:11 -0500

    update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,7 +3,10 @@
 
 
 class Square:
-
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
         """initializes the square
         Args:
