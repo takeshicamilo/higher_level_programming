176b2dfb2eb467d5da15c771c81671bb97920471
Lilsherlock <potatogames15@gmail.com>
2020-09-25 21:45:59 -0500

    update

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" 4-print_square.py """
+
+
 def print_square(size):
+    """ print square """
     if type(size) is int:
         if size < 0:
             raise ValueError("size must be >= 0")
