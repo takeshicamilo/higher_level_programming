8f7c206f12443ee80cc44787e58031f3fa898f23
Lilsherlock <potatogames15@gmail.com>
2020-09-25 21:44:33 -0500

    update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""say_my_name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """ say my name function"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    first_name += " " +last_name
+    first_name += " " + last_name
     print("My name is", first_name)
