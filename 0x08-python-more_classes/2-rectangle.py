08f845156d0f8545bbd0c0b029b0e8495ccdc57b
Lilsherlock <potatogames15@gmail.com>
2020-09-28 20:26:14 -0500

    update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,6 +3,7 @@
 
 
 class Rectangle:
+    """ Rectangle """
     def __init__(self, width=0, height=0):
         """ init method """
         self.width = width
@@ -31,9 +32,9 @@ class Rectangle:
     def height(self, value):
         """ height method """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
