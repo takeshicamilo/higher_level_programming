18ac3e51015daf4790cdec094fa6c2caae60ea90
lilsherlock <potatogames15@gmail.com>
2020-10-07 23:35:36 +0000

    update

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" read_file """
+
+
+def read_file(filename=""):
+    """ read file function """
+    with open(filename, encoding="utf-8") as a_file:
+        for a_line in a_file:
+            print("{}".format(a_line), end="")
+        print("")
