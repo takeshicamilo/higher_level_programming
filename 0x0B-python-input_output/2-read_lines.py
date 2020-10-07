ef478867f6e5714f75876f838523b932d356072c
lilsherlock <potatogames15@gmail.com>
2020-10-07 23:55:42 +0000

    update

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,13 +1,24 @@
-"""This is the documentation for this module. Just read a filename, and
-prints its content given a number of lines"""
+""" Module that contains a function that reads n lines of a text file
+"""
 
 
 def read_lines(filename="", nb_lines=0):
-    """read line function """
-    with open(filename, 'r') as f:
-        i = 0
-        for lines in f:
-            if nb_lines > 0 and nb_lines <= i:
-                break
-            i += 1
-            print("{}".format(lines), end="")
+    """ Function that reads from a file and prints its number of lines
+    Args:
+        filename: filename
+        nb_lines: number of lines to print
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        if nb_lines <= 0:
+            read_data = f.read()
+            print(read_data, end='')
+        else:
+            n_lines = 0
+            for line in f:
+                print(line, end='')
+                n_lines += 1
+                if n_lines == nb_lines:
+                    break
