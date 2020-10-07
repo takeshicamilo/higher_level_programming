a1917c2973b2c7d6bb0e80559da744650adf95a7
lilsherlock <potatogames15@gmail.com>
2020-10-07 23:45:04 +0000

    update

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 1-number_of_lines """
+def number_of_lines(filename=""):
+    """ number of lines function """
+    lines_number = 0
+
+    with open(filename, encoding="utf-8") as a_file:
+        for a_line in a_file:
+            lines_number += 1
+    return lines_number
