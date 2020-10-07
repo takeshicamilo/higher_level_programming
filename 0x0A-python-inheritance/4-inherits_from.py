fa1d5146d4dee1c9303bd90d631340c405f734cc
lilsherlock <potatogames15@gmail.com>
2020-10-07 00:26:37 +0000

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """ inherits_from function """
-    return(issubclass(type(obj), a_class) and not isinstance(obj, a_class))
+    return(issubclass(type(obj), a_class) and type(obj) != a_class)
