e69d26105c7f501528fbb8b440048537d2507f02
lilsherlock <potatogames15@gmail.com>
2020-10-07 23:59:14 +0000

    update

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" write file """
+
+
+def write_file(filename="", text=""):
+    """ write file function """
+    length = len(text)
+    with open(filename, "w" , encoding="utf-8") as file:
+        file.write(text)
+    return length
