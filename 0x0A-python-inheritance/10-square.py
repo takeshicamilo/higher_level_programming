6bfb5769af4f885382e2eb8e18d2b79a0ecf2022
lilsherlock <potatogames15@gmail.com>
2020-10-07 01:54:02 +0000

    update

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,6 +5,7 @@ This module defines a class Square
 """
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """ Square class """
 
@@ -13,7 +14,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-    
+
     def area(self):
         """ area function """
         return self.__size ** 2
