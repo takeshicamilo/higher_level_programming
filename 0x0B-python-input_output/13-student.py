5163f9000e79389b319766b0b2a7d1c3940009e0
lilsherlock <potatogames15@gmail.com>
2020-10-08 00:08:36 +0000

    update

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" students class """
+
+
+class Student():
+    """ class student """
+
+    def __init__(self, first_name, last_name, age):
+        """init function """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to json function"""
+        if type(attrs) is not list:
+            return self.__dict__
+        for element in attrs:
+            if type(element) is not str:
+                return self.__dict__
+        return {key: value for (key, value)
+                in self.__dict__.items() if key in attrs}
+    
+    def reload_from_json(self, json):
+        """ reload from json function """
+        self.__dict__ = json
