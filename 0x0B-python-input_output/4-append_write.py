640ae6039dd6f791f24e6d8f4fde5423c5de5bd1
lilsherlock <potatogames15@gmail.com>
2020-10-08 00:01:42 +0000

    update

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+""" append write """
 
 
 def append_write(filename="", text=""):
     """ write file function """
-    length = len(text)
-    with open(filename, "a" , encoding="utf-8") as file:
-        file.write(text)
+    with open(filename, "a", encoding="utf-8") as file:
+        length = file.write(text)
     return length
