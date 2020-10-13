fb454b2a9ec2e6379b615becee5543e52fad47f3
Lilsherlock <potatogames15@gmail.com>
2020-10-13 14:47:03 -0500

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base():
     def to_json_string(list_dictionaries):
         """ to json string method """
 
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         json_string = json.dumps(list_dictionaries)
         return json_string
