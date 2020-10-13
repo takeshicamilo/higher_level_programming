cca3852a98ee78ea501e2d0afceff5c7cce0458a
Lilsherlock <potatogames15@gmail.com>
2020-10-13 14:51:08 -0500

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,12 +17,12 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ to json string method """
+        """ to json method """
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
+            return '[]'
 
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
