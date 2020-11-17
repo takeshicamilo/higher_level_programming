8c3f2496f644d75e725007b4a6760438e9f6af13
lilsherlock <potatogames15@gmail.com>
2020-11-17 19:30:26 +0000

    update

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates the database hbtn_0d_usa
+-- creates the table hbtn_0d_usa
+
+CREATE DATABASE hbtn_0d_usa;
+CREATE TABLE states (id INT unique PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL)
