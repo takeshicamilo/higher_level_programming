afb14a4069328de6096197cb6b1f38c09aab9dfc
lilsherlock <potatogames15@gmail.com>
2020-11-17 17:21:43 +0000

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of mySQL user
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
 
