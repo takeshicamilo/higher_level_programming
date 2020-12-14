51ac6b7dffe8563f2c59d736d1dfa070f0da0edf
lilsherlock <potatogames15@gmail.com>
2020-12-14 06:54:40 -0500

    update

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-model_city 
+model_city
 """
 
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -8,13 +8,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class City(Base):
-        """Represents a state for a MySQL database.
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        """
-        __tablename__ = "cities"
-        id = Column(Integer, primary_key=True)
-        name = Column(String(128), nullable=True)
-        state_id = Column(Integer, ForeignKey("states.id"), nullable=True)
+    """Represents a state for a MySQL database.
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=True)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=True)
