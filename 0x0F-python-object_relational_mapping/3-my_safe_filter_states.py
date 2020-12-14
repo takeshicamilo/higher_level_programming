20e6064af0be64181952ca5048b252d771387ee4
lilsherlock <potatogames15@gmail.com>
2020-12-13 23:51:35 -0500

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,11 +6,14 @@ all states in orderfrom the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
 
-cur.execute(
-    "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4]))
 
-for state in cur.fetchall():
-    print(state)
+    for state in cur.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
