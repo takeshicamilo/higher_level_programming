dd9ca857e9f76ad8d932c624eae278674bec03ac
lilsherlock <potatogames15@gmail.com>
2020-12-14 00:01:12 -0500

    update

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-#xdd
+"""
+changes the name of the State object where id=2 to New Mexico from a database
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,13 +11,13 @@ import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
-        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        for states in session.query(State).order_by(State.id):
-                if 'a' in states.name:
-                        session.delete(states)
-        session.commit()
+    for states in session.query(State).order_by(State.id):
+        if 'a' in states.name:
+            session.delete(states)
+    session.commit()
