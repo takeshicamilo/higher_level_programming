1cc62ae4f3bd1b7b072c3596fb77a5c97fdb9ae4
lilsherlock <potatogames15@gmail.com>
2020-12-13 23:42:14 -0500

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-lists all states with N from the database hbtn_0e_0_usa
+python script that lists all states from the database hbtn_0e_0_usa with a name
+starting with N
 """
 
 import MySQLdb
 import sys
 
 db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
+cursor = db.cursor()
+ cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+  rows = cursor.fetchall()
+   for row in rows:
         if row[1][0] == 'N':
             print(row)
     cursor.close()
