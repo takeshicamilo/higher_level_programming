df236250653909d307bf40663622cf900f8691a3
lilsherlock <potatogames15@gmail.com>
2020-12-13 23:57:51 -0500

    update

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-#xsddd
+"""
+list the first State object from a database
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,14 +11,14 @@ import sys
 from model_state import Base, State
 
 if __name__ == "__main__":
-        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        states = session.query(State).order_by(State.id).first()
-        if states is None:
-                print("Nothing")
-        else:
-                print("{}: {}".format(states.id, states.name))
+    states = session.query(State).order_by(State.id).first()
+    if states is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(states.id, states.name))
