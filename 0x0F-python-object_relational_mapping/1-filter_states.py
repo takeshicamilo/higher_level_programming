8d7a4ea231a611109c611fdc73675a7a04ffd540
lilsherlock <potatogames15@gmail.com>
2020-12-13 23:41:15 -0500

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,11 @@ import MySQLdb
 import sys
 
 db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states WHERE name REGEXP '^[n]'")
-
-for state in cur.fetchall():
-    print(state)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
+    db.close()
