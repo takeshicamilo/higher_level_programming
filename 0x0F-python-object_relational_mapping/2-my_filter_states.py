d1bb38152fa9aa34453273726481a99ac0817bba
lilsherlock <potatogames15@gmail.com>
2020-12-13 23:47:31 -0500

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,22 @@
+
 #!/usr/bin/python3
 """
-script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa
+python script that lists all states from the database hbtn_0e_0_usa with
+a given name
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
-
-for state in cur.fetchall():
-    print(state)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    db.close()
