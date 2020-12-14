4aa71889711add12d4648fd5cb6ddc2c3f5d5760
lilsherlock <potatogames15@gmail.com>
2020-12-14 03:57:44 +0000

    update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#xdd
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        states = session.query(State).filter_by(id=2).first()
+        states.name = 'New Mexico'
+
+        session.commit()
