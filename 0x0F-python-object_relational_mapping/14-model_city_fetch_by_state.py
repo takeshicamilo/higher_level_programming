a6767cd07014a78fc09057ba3474ad0ebd882f49
lilsherlock <potatogames15@gmail.com>
2020-12-14 00:02:27 -0500

    update

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#xdd
+"""
+prints all City objects from a database
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,11 +11,14 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-        engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        for city, state in session.query(City, State).filter(City.state_id == State.id).order_by(City.id):
-                print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for city, state in session.query(
+        City, State).filter(
+        City.state_id == State.id).order_by(
+            City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
