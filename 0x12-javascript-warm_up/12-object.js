a4f3f830eb2c40ef4d3efcd82eccc4649d971f0b
xd <xd@xds-MacBook-Pro.local>
2021-02-02 12:11:21 -0500

    update

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -2,11 +2,11 @@
 // xddd
 
 const myObject = {
-  type: 'object',
-  value: 12
-};
+    type: 'object',
+    value: 12
+}
 
-console.log(myObject);
+console.log(myObject)
 
-myObject.value = 89;
-console.log(myObject);
+myObject.value = 89
+console.log(myObject)
