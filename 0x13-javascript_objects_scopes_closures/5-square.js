fcbb6cf0d3bf21e7ae7c7e380d5a61cdb2e0dbf0
xd <xd@xds-MacBook-Pro.local>
2021-02-08 14:59:56 -0500

    update

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,9 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 // xdddd
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 };
