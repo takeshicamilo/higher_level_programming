9c46a6edb3fc69079208ee13bfc9594e89f468a3
lilsherlock <potatogames15@gmail.com>
2021-02-09 13:59:53 -0500

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,24 @@
 #!/usr/bin/node
-
-// Read the file and print its contents.
-const link = process.argv[2];
+/*
+    prints the number of movies where the
+    character “Wedge Antilles” is present.
+*/
 const request = require('request');
-let temp = 0;
-const name = 'https://swapi-api.hbtn.io/api/people/18/';
+const url = process.argv[2];
 
-request(link, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const jsonDict = JSON.parse(body);
-
-    for (let i = 0; i < jsonDict.results.length; i++) {
-      for (let x = 0; x < jsonDict.results[i].characters.length; x++) {
-        if (name === jsonDict.results[i].characters[x]) {
-          temp++;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('code:', response.statusCode);
+  } else {
+    const jsonBody = JSON.parse(body);
+    let count = 0;
+    for (const film of jsonBody.results) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) { // ...people/18 is Wedge Antilles
+          count += 1;
         }
       }
     }
-    console.log(temp);
+    console.log(count);
   }
 });
